52e112c78e08894e07956cb49ff0e938fe78ed73
StormBlade13 <zacharykbrown12@outlook.com>
2026-04-20 15:09:37 -0500

    Zach: Changed getGradesArray to return a numpy array

--- data_handler.py
+++ data_handler.py
@@ -21,6 +21,9 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "database.db")
 
+#Imports numpy
+import numpy as np
+
 #Creates database class
 Base = declarative_base()
 
@@ -247,11 +250,19 @@ def delete_password_row(student_id):
     finally:
         session.close()
 
-def gradesToTuple():
+def getGradesArray():
 
     session = Session()
 
     statement = sa.select(StudentTable.grade)
-    column = session.execute(statement).all()
+    rows = session.execute(statement).all()
 
-    return column
+    grade_lists = []
+
+    for (grade_str,) in rows:
+        grades = [int(x) for x in grade_str.split(",")]
+        grade_lists.append(grades)
+
+    arr = np.array(grade_lists)
+
+    return arr
