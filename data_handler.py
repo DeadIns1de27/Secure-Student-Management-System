13591a1f63be0b795fcb2e839f051b7c6a5fd0d9
DeadIns1de27 <marcuschow27@gmail.com>
2026-04-21 22:47:01 -0500

    Added validate login for use in gui.py

--- data_handler.py
+++ data_handler.py
@@ -10,6 +10,9 @@ import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.exc import IntegrityError
 
+#Import password hash verification
+from security import verify_password
+
 #Import Data Layer
 from student import StudentRecord
 
@@ -244,6 +247,38 @@ def delete_password_row(student_id):
         session.rollback()
         return {"success": False, "error": str(e)}
     
+    finally:
+        session.close()
+
+#checks the user password and also their status(admin / student)
+def validate_login(student_id, input_password) -> str:
+    session = Session()
+
+    try:
+        #loads user password associated with id
+        password = load_password(student_id)
+
+        #check if theres a password
+        if not password:
+            return None
+        
+        #check if the input password matches the stores password
+        if not verify_password(password, input_password):
+            return None
+        
+        #get the adminstatus of the id from the database
+        query = sa.select(StudentTable.adminStatus).where(StudentTable.studentID == student_id)
+        adminStatus = session.execute(query).scalar_one_or_none()
+
+        #check if user is admin or student
+        if adminStatus:
+            return "Admin"
+        return "Student"
+
+    except Exception as e:
+        session.rollback()
+        return None
+
     finally:
         session.close()
 
